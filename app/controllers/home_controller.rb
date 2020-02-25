4652d85b9dab76ec3cea5bec29c0294d27d8ba63
George Dong <dongsy326@gmail.com>
2020-02-25 18:16:39 -0500

    formatted header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,11 @@ class HomeController < ApplicationController
   def index
     @themes = Theme.all
     @editMode = true
+    @themeClass = "non-edit-mode"
+
+    if @editMode
+      @themeClass = "edit-mode"
+    end
   end
+
 end
