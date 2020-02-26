67944e0889dd9669bcacf84fa4a3bf19589428f0
George Donald <george.sy.donald@gmail.com>
2020-02-25 20:10:44 -0800

    added onload event handler

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,3 +5,8 @@ function onThemeSelChanged(id){
 function onEditTheme() {
     alert("Editing")
 }
+
+window.onload = () => {
+    let ele = document.getElementById('theme_dropdown');
+    onThemeSelChanged(ele.value)
+}
