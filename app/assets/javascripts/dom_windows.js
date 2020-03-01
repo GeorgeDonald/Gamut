9c4b1e03063f571f7219171584ecef6192d210b5
George Donald <george.sy.donald@gmail.com>
2020-03-01 01:53:46 -0500

    added Color class and it's test set

--- app/assets/javascripts/dom_windows.js
+++ app/assets/javascripts/dom_windows.js
@@ -1,5 +1,9 @@
 ////////////////////////////////////////////////////////////////////
 // global functions
+function log(msg){
+  console.log(msg);
+}
+
 function toNumber(x){
   if(typeof x == 'string') x = parseInt(x);
   if(typeof x != 'number') x = 0;
@@ -158,6 +162,262 @@ class Rect {
     return equals(["left", "top", "right", "bottom"], this, new Rect(left, top, right, bottom));
   }
 }
+
+class Color{
+  static predefinedColors = {
+    aliceblue: [240,248,255],
+    antiquewhite: [250,235,215],
+    aqua: [0,255,255],
+    aquamarine: [127,255,212],
+    azure: [240,255,255],
+    beige: [245,245,220],
+    bisque: [255,228,196],
+    black: [0,0,0],
+    blanchedalmond: [255,235,205],
+    blue: [0,0,255],
+    blueviolet: [138,43,226],
+    brown: [165,42,42],
+    burlywood: [222,184,135],
+    cadetblue: [95,158,160],
+    chartreuse: [127,255,0],
+    chocolate: [210,105,30],
+    coral: [255,127,80],
+    cornflowerblue: [100,149,237],
+    cornsilk: [255,248,220],
+    crimson: [220,20,60],
+    cyan: [0,255,255],
+    darkblue: [0,0,139],
+    darkcyan: [0,139,139],
+    darkgoldenrod: [184,134,11],
+    darkgray: [169,169,169],
+    darkgrey: [169,169,169],
+    darkgreen: [0,100,0],
+    darkkhaki: [189,183,107],
+    darkmagenta: [139,0,139],
+    darkolivegreen: [85,107,47],
+    darkorange: [255,140,0],
+    darkorchid: [153,50,204],
+    darkred: [139,0,0],
+    darksalmon: [233,150,122],
+    darkseagreen: [143,188,143],
+    darkslateblue: [72,61,139],
+    darkslategray: [47,79,79],
+    darkslategrey: [47,79,79],
+    darkturquoise: [0,206,209],
+    darkviolet: [148,0,211],
+    deeppink: [255,20,147],
+    deepskyblue: [0,191,255],
+    dimgray: [105,105,105],
+    dimgrey: [105,105,105],
+    dodgerblue: [30,144,255],
+    firebrick: [178,34,34],
+    floralwhite: [255,250,240],
+    forestgreen: [34,139,34],
+    fuchsia: [255,0,255],
+    gainsboro: [220,220,220],
+    ghostwhite: [248,248,255],
+    gold: [255,215,0],
+    goldenrod: [218,165,32],
+    gray: [128,128,128],
+    grey: [128,128,128],
+    green: [0,128,0],
+    greenyellow: [173,255,47],
+    honeydew: [240,255,240],
+    hotpink: [255,105,180],
+    indianred: [205,92,92],
+    indigo: [75,0,130],
+    ivory: [255,255,240],
+    khaki: [240,230,140],
+    lavender: [230,230,250],
+    lavenderblush: [255,240,245],
+    lawngreen: [124,252,0],
+    lemonchiffon: [255,250,205],
+    lightblue: [173,216,230],
+    lightcoral: [240,128,128],
+    lightcyan: [224,255,255],
+    lightgoldenrodyellow: [250,250,210],
+    lightgray: [211,211,211],
+    lightgrey: [211,211,211],
+    lightgreen: [144,238,144],
+    lightpink: [255,182,193],
+    lightsalmon: [255,160,122],
+    lightseagreen: [32,178,170],
+    lightskyblue: [135,206,250],
+    lightslategray: [119,136,153],
+    lightslategrey: [119,136,153],
+    lightsteelblue: [176,196,222],
+    lightyellow: [255,255,224],
+    lime: [0,255,0],
+    limegreen: [50,205,50],
+    linen: [250,240,230],
+    magenta: [255,0,255],
+    maroon: [128,0,0],
+    mediumaquamarine: [102,205,170],
+    mediumblue: [0,0,205],
+    mediumorchid: [186,85,211],
+    mediumpurple: [147,112,219],
+    mediumseagreen: [60,179,113],
+    mediumslateblue: [123,104,238],
+    mediumspringgreen: [0,250,154],
+    mediumturquoise: [72,209,204],
+    mediumvioletred: [199,21,133],
+    midnightblue: [25,25,112],
+    mintcream: [245,255,250],
+    mistyrose: [255,228,225],
+    moccasin: [255,228,181],
+    navajowhite: [255,222,173],
+    navy: [0,0,128],
+    oldlace: [253,245,230],
+    olive: [128,128,0],
+    olivedrab: [107,142,35],
+    orange: [255,165,0],
+    orangered: [255,69,0],
+    orchid: [218,112,214],
+    palegoldenrod: [238,232,170],
+    palegreen: [152,251,152],
+    paleturquoise: [175,238,238],
+    palevioletred: [219,112,147],
+    papayawhip: [255,239,213],
+    peachpuff: [255,218,185],
+    peru: [205,133,63],
+    pink: [255,192,203],
+    plum: [221,160,221],
+    powderblue: [176,224,230],
+    purple: [128,0,128],
+    rebeccapurple: [102,51,153],
+    red: [255,0,0],
+    rosybrown: [188,143,143],
+    royalblue: [65,105,225],
+    saddlebrown: [139,69,19],
+    salmon: [250,128,114],
+    sandybrown: [244,164,96],
+    seagreen: [46,139,87],
+    seashell: [255,245,238],
+    sienna: [160,82,45],
+    silver: [192,192,192],
+    skyblue: [135,206,235],
+    slateblue: [106,90,205],
+    slategray: [112,128,144],
+    slategrey: [112,128,144],
+    snow: [255,250,250],
+    springgreen: [0,255,127],
+    steelblue: [70,130,180],
+    tan: [210,180,140],
+    teal: [0,128,128],
+    thistle: [216,191,216],
+    tomato: [255,99,71],
+    turquoise: [64,224,208],
+    violet: [238,130,238],
+    wheat: [245,222,179],
+    white: [255,255,255],
+    whitesmoke: [245,245,245],
+    yellow: [255,255,0],
+    yellowgreen: [154,205,50],
+  }
+
+  static GetColorValue(c){
+    if(typeof c == "string") {
+      let re = /^\s*#([0-9a-fA-F]{1,2})\s*$/g;
+      let rlt = re.exec(c);
+      if(rlt && rlt.length == 2){
+        return parseInt(rlt[1], 16);
+      } else c = parseInt(c);
+    }
+
+    if(typeof c == "number") return c>255?255:c<0?0:c;
+    else return 0;
+  }
+
+  static New(red, green, blue, alpha) {
+    return new Color(red, green, blue, alpha);
+  }
+
+  _red = 0
+  _green = 0
+  _blue = 0
+  _alpha = 0
+
+  constructor(red, green, blue, alpha){
+    if(typeof red == "string"){
+      let re = /^\s*#{0,1}([0-9a-fA-F]{2,8})\s*$/g;
+      let rlt = re.exec(red);
+      if(rlt && rlt.length == 2){
+        if(rlt[1].length==2) rlt[1] = rlt[1] + rlt[1] + rlt[1];
+        else if(rlt[1].length==3) rlt[1] = rlt[1] + rlt[1].split("").reverse().join("");
+        for(;rlt[1].length < 8;) rlt[1] = "0" + rlt[1];
+
+        this._alpha = parseInt(rlt[1].substring(0,2), 16);
+        this._red = parseInt(rlt[1].substring(2,4), 16);
+        this._green = parseInt(rlt[1].substring(4,6), 16);
+        this._blue = parseInt(rlt[1].substring(6), 16);
+        return;
+      } else {
+        let clr = Color.predefinedColors[red.toLowerCase()];
+        if(clr){
+          this._red = clr[0];
+          this._green = clr[1];
+          this._blue = clr[2];
+          return;
+        }
+      }
+    }
+
+    let t = toNumberObject(["red", "green", "blue", "alpha"], red, green, blue, alpha);
+    this._red = t.red;
+    this._green = t.green;
+    this._blue = t.blue;
+    this._alpha = t.alpha;
+  }
+
+  get red(){
+    return this._red;
+  }
+  get r(){
+    return this.red;
+  }
+  set red(r){
+    this._red = Color.GetColorValue(r);
+  }
+  set r(r){
+    this.red = r;
+  }
+  get green(){
+    return this._green;
+  }
+  get g(){
+    return this.green;
+  }
+  set green(g){
+    this._green = Color.GetColorValue(g);
+  }
+  set g(g){
+    this.green = g;
+  }
+  get blue(){
+    return this._blue;
+  }
+  get b(){
+    return this.blue;
+  }
+  set blue(b){
+    this._blue = Color.GetColorValue(b);
+  }
+  set b(b){
+    this.blue = b;
+  }
+  get alpha(){
+    return this._alpha;
+  }
+  get a(){
+    return this.alpha;
+  }
+  set alpha(a){
+    this._alpha = Color.GetColorValue(a);
+  }
+  set a(a){
+    this.alpha = a;
+  }
+}
 ////////////////////////////////////////////////////////////////////
 
 
@@ -302,6 +562,10 @@ class domWnd {
     if(!m) return;
     this.element.style.height = m.descript;
   }
+  get bgClr(){
+    //re = /^rgba*\((\d{1,3}), (\d{1,3}), (\d{1,3}),* *(\d*)\)$/g
+    return this.element.style.backgroundColor;
+  }
   // for this base wnd class, create a "div" wnd
   // unless parent is null, in which case set this wnd to html body
   Create(parent){
@@ -315,9 +579,9 @@ class domWnd {
 
     // this is already a valid wnd, return true
     if(this.wndValid) return true;
+    if(!domWnd.IsValidWnd(parent)) return false;
 
-    if(!this.pageValid || !domWnd.IsValidWnd(parent)) return false;
-
+    this._page = parent.page;
     this._id = this.page.newId;
     this._parent = parent;
     let thisElement = document.createElement("div");
@@ -329,3 +593,10 @@ class domWnd {
     return true;
   }
 }
+
+
+
+
+
+
+
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -25,19 +25,99 @@ function onEditTheme() {
     }
 }
 
-window.onload = () => {
-    let ele = document.getElementById('theme_dropdown');
-    if(ele) {
-        onThemeSelChanged(ele.value)
+window.onload = onLoad;
+function onLoad(){
+    // let ele = document.getElementById('theme_dropdown');
+    // if(ele) {
+    //     onThemeSelChanged(ele.value)
+    // }
+    for(;document.body.children.length>0;){
+        document.body.removeChild(document.body.children[0]);
     }
 
-    let page = domPage.New();
-    console.log(`width: ${page.mainWnd.width}, height: ${page.mainWnd.height}`)
+    let clr = new Color(10, 20, 30, 40);
+    if(clr.r != 10) log("error")
+    if(clr.g != 20) log("error")
+    if(clr.b != 30) log("error")
+    if(clr.a != 40) log("error")
 
-    let nwnd = new domWnd(page);
-    nwnd.Create(page.mainWnd)
-    nwnd.width = "80px"
-    console.log(`new created window's id is ${nwnd.id}`)
-    ele = document.getElementById(nwnd.id);
-    ele.innerText = ("this is a window created by domWnd")
+    clr = new Color({red: 11, green: 21, blue: 31, alpha: 41});
+    if(clr.r != 11) log("error")
+    if(clr.g != 21) log("error")
+    if(clr.b != 31) log("error")
+    if(clr.a != 41) log("error")
+
+    clr = Color.New(new Color({red: 111, green: 121, blue: 131, alpha: 141}));
+    if(clr.r != 111) log("error")
+    if(clr.g != 121) log("error")
+    if(clr.b != 131) log("error")
+    if(clr.a != 141) log("error")
+
+    clr = Color.New("white");
+    if(clr.r != 255) log("error")
+    if(clr.g != 255) log("error")
+    if(clr.b != 255) log("error")
+    if(clr.a != 0) log("error")
+
+    Object.keys(Color.predefinedColors).forEach(cn => {
+        clr = Color.New(cn);
+        if(clr.r != Color.predefinedColors[cn][0]) log("error")
+        if(clr.g != Color.predefinedColors[cn][1]) log("error")
+        if(clr.b != Color.predefinedColors[cn][2]) log("error")
+        if(clr.a != 0) log("error")
+    
+    })
+
+    clr.r = 101;
+    if(clr.r != 101) log("error 101")
+
+    clr.g = 201;
+    if(clr.g != 201) log("error 201")
+
+    clr.b = 301;
+    if(clr.b != 255) log(`error 301 : ${clr.b}`)
+
+    clr.a = 401;
+    if(clr.a != 255) log("error 401")
+
+    clr = Color.New("#ff");
+    if(clr.r != 255) log("error #ff 1")
+    if(clr.g != 255) log("error #ff 2")
+    if(clr.b != 255) log("error #ff 3")
+    if(clr.a != 0) log("error #ff a")
+
+    clr = Color.New("2f");
+    if(clr.r != 0x2f) log("error #2f 1")
+    if(clr.g != 0x2f) log("error #2f 2")
+    if(clr.b != 0x2f) log("error #2f 3")
+    if(clr.a != 0) log("error #2f a")
+
+    clr = Color.New("af2");
+    if(clr.r != 0xaf) log("error af2 1")
+    if(clr.g != 0x22) log("error af2 2")
+    if(clr.b != 0xfa) log("error af2 3")
+    if(clr.a != 0) log("error af2 a")
+
+    clr = Color.New("#af2");
+    if(clr.r != 0xaf) log("error #af2 1")
+    if(clr.g != 0x22) log("error #af2 2")
+    if(clr.b != 0xfa) log("error #af2 3")
+    if(clr.a != 0) log("error #af2 a")
+
+    clr = Color.New("#1f2f3f");
+    if(clr.r != 0x1f) log("error #1f2f3f 1")
+    if(clr.g != 0x2f) log("error #1f2f3f 2")
+    if(clr.b != 0x3f) log("error #1f2f3f 3")
+    if(clr.a != 0) log("error #1f2f3f a")
+
+    // let page = domPage.New();
+    // console.log(`width: ${page.mainWnd.width}, height: ${page.mainWnd.height}`)
+
+    // let nwnd = new domWnd();
+    // nwnd.Create(page.mainWnd)
+    // nwnd.width = "80px"
+    // console.log(`new created window's id is ${nwnd.id}`)
+    // ele = document.getElementById(nwnd.id);
+    // ele.innerText = ("this is a window created by domWnd")
+    log("Done")
   }
